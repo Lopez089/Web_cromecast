c27e16c12f90c86f539c8480df30fcc3b94d592b
Juan Lopez <contacto@juanlopezaragon.es>
2018-09-17 01:17:58 +0200

    v.1.2

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ function abrir() {
 function cerrar() {
 	document.getElementById("abrir").style.visibility="hidden";
 };
+function abrirauto() {
+	setTimeout(abrirFeedback, 10000);
+	function abrirFeedback() {
+		document.getElementById("feedback").style.visibility="visible";
+	}
+}
+abrirauto();
+function abrirComentario() {
+	document.getElementById("feedbackText").style.visibility="visible";
+	document.getElementById("feedback").style.visibility="hidden";
+};
+function cerrarComen() {
+	document.getElementById("feedbackText").style.visibility="hidden";
+};
